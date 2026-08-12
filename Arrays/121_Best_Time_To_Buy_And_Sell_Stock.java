bf75c9636d28c89988b19a90eca4407ed1e4fa50
Vinay Kumar <vinayjeet92@gmail.com>
2026-08-13 00:44:52 +0530

    Add LeetCode 121 solution

---
+++ Arrays/121_Best_Time_To_Buy_And_Sell_Stock.java
@@ -0,0 +1,17 @@
+// LeetCode 121 - Best Time to Buy and Sell Stock
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
